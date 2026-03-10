523cd334d28db2c05b1baf2bb2a368f1c40b3f03
ibbisulaiman <ibbisulaiman@gmail.com>
2026-03-11 00:02:38 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,31 +1,5 @@
-// Example live scores
-const matches = [
-
-{
-team1:"Barcelona",
-team2:"Real Madrid",
-score:"2 - 1"
-},
-
-{
-team1:"Manchester City",
-team2:"Arsenal",
-score:"1 - 1"
+fetch("https://v3.football.api-sports.io/standings?league=39&season=2026",{
+headers:{
+"x-apisports-key": apiKey
 }
-
-]
-
-let container = document.getElementById("liveScores")
-
-matches.forEach(match => {
-
-let div = document.createElement("div")
-
-div.innerHTML = `
-<h3>${match.team1} vs ${match.team2}</h3>
-<p>${match.score}</p>
-`
-
-container.appendChild(div)
-
 })
