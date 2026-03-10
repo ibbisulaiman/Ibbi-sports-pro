8120c0781d76d8a1d70a16fbb1e2d53416af41a2
ibbisulaiman <ibbisulaiman@gmail.com>
2026-03-11 00:28:52 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
-fetch("https://v3.football.api-sports.io/standings?league=39&season=2026",{
+const apiKey = "df1f3c45122c328abb92ee7a48c59804b57a9267cb1c15df8170bb8107e59326";
+
+fetch("https://v3.football.api-sports.io/fixtures?live=all",{
 headers:{
 "x-apisports-key": apiKey
 }
 })
+
+.then(response => response.json())
+
+.then(data => {
+
+let matches = data.response;
+
+let container = document.getElementById("liveScores");
+
+container.innerHTML = "";
+
+matches.forEach(match => {
+
+let div = document.createElement("div");
+
+div.innerHTML = `
+<h3>${match.teams.home.name} vs ${match.teams.away.name}</h3>
+<p>${match.goals.home} - ${match.goals.away}</p>
+`;
+
+container.appendChild(div);
+
+});
+
+});
